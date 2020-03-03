5b319d3d5fd98faea90d2bd61e6b3cae255bba94
bels4 <belsalovephysics@gmail.com>
2020-03-03 17:41:44 +0300

    123

--- download_script.sh
+++ download_script.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
 
-echo "Hello! I am downloading data. Please wait."
-PREFIX=$1
-BEGIN=$2
-END=$3
+FLAG=$1
+DIR=$2
+PREFIX=$3
+BEGIN=$4
+END=$5
 
-for (( VAR=$BEGIN; VAR<=$END; VAR++ ))
-do
-	touch $PREFIX$VAR.gb
-	efetch -db nucleotide -format gb -id $PREFIX$VAR > $PREFIX$VAR.gb
-done
+case $FLAG in
+	-gb)
+		echo "Starting to work"
+		
+		if [ ! -d "$DIR" ]
+		then
+			mkdir $DIR
+		fi
+
+		echo "Downloading from GenBank"
+
+		for (( VAR=$BEGIN; VAR<=$END; VAR++ ))
+		do
+			echo "Downloading file $PREFIX$VAR.gb"
+			touch $DIR/$PREFIX$VAR.gb
+			efetch -db nucleotide -format gb -id $PREFIX$VAR > $DIR/$PREFIX$VAR.gb
+		done
+	;;
+	-lanl)
+		echo "Starting to work"
+		
+		if [ ! -d "$DIR" ]
+		then
+			mkdir $DIR
+		fi
+
+		echo "Downlaoding from HIV LANL"
+
+		for (( VAR=$BEGIN; VAR<=$END; VAR++ ))
+		do
+			echo "Downloading file $PREFIX$VAR.gb"
+			touch $DIR/$PREFIX$VAR.gb
+			wget -nv -O "$DIR/$PREFIX$VAR.gb" "https://www.hiv.lanl.gov/tmp/argo/CdMDCjY_/$PREFIX$VAR-new1.gb"
+		done
+	;;
+	*)
+		cat < help.txt 
+	;;
+esac
